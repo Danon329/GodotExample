d88ca90d3b9a51a8b6a4025a646174a9fcc54967
Daniel Feldman <daniel.salit@hotmail.com>
2025-11-15 00:38:51 +0100

    Implemented small animations (Improve later)

--- beispiel_szenen/spieler/beispiel_spieler.gd
+++ beispiel_szenen/spieler/beispiel_spieler.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var anim_player: AnimationPlayer = $AnimationPlayer
 
 @export var gravity: float = 400.0
 @export var break_factor: float = 20.0
@@ -9,12 +10,12 @@ extends CharacterBody2D
 const JUMP_HEIGHT: float = -300.0
 
 var direction: float = 0
-var anim_playing: bool = false
+var is_walking: bool = false
+var is_in_air: bool = false
 
 func _physics_process(delta: float) -> void:
 	apply_gravity(delta)
 	apply_break()
-	check_for_idle()
 	
 	movement()
 	move_and_slide()
@@ -23,19 +24,28 @@ func _physics_process(delta: float) -> void:
 func apply_gravity(delta: float) -> void:
 	if !is_on_floor():
 		velocity.y += gravity * delta
+	elif is_on_floor() and is_in_air:
+		is_in_air = false
+		if is_walking:
+			anim_player.play("walk")
+		else:
+			anim_player.play("idle")
 
+
+#func apply_break() -> void:
+#	if !is_equal_approx(velocity.x, 0) and Input.get_axis("links", "rechts") == 0:
+#		velocity.x += break_factor * -direction
+#	else:
+#		velocity.x = 0
 
 func apply_break() -> void:
-	if !is_equal_approx(velocity.x, 0) and Input.get_axis("links", "rechts") == 0:
-		velocity.x += break_factor * -direction
-	else:
+	if velocity.x < -10 or velocity.x > 10:
+		velocity.x += break_factor * -direction # improve math behind break
+	elif velocity.x > -10 and velocity.x < 10:
 		velocity.x = 0
-
-
-func check_for_idle() -> void:
-	if is_on_floor() and velocity == Vector2.ZERO:
-		sprite.play("idle")
-		anim_playing = false
+		if is_on_floor():
+			anim_player.play("idle")
+			is_walking = false
 
 
 func movement() -> void:
@@ -50,13 +60,16 @@ func walk() -> void:
 		direction = dir
 		velocity.x = speed * direction
 		flip_sprite()
-		play_anim("walk")
+		if !is_walking and is_on_floor():
+			anim_player.play("walk")
+			is_walking = true
 
 
 func jump() -> void:
 	if Input.is_action_pressed("springen") and is_on_floor():
 		velocity.y = JUMP_HEIGHT
-		play_anim("jump");
+		anim_player.play("jump")
+		is_in_air = true
 	elif Input.is_action_just_released("springen"):
 		velocity.y = 0
 
@@ -67,10 +80,3 @@ func flip_sprite() -> void:
 	elif velocity.x < 0:
 		sprite.flip_h = true
 
-
-func play_anim(animation: String): # rework animation system
-	if !anim_playing:
-		sprite.play(animation)
-		anim_playing = true
-	else:
-		return
